9be83b7a267c0c68003346657d9780f33fee2704
Sushant <patrikarss@rknec.edu>
2020-08-24 20:35:48 +0530

    reformatted code

--- price_comparison_engine.py
+++ price_comparison_engine.py
@@ -6,9 +6,11 @@ import webbrowser
 from collections import defaultdict
 
 root = Tk()
+
+
 class Price_compare:
 
-    def __init__(self,master):
+    def __init__(self, master):
         self.var = StringVar()
         self.var_ebay = StringVar()
         self.var_flipkart = StringVar()
@@ -20,7 +22,7 @@ class Price_compare:
         entry = Entry(master, textvariable=self.var)
         entry.grid(row=0, column=1)
 
-        button_find = Button(master, text='Find', bd=4,command=self.find)
+        button_find = Button(master, text='Find', bd=4, command=self.find)
         button_find.grid(row=1, column=1, sticky=W, pady=8)
 
     def find(self):
@@ -33,37 +35,33 @@ class Price_compare:
         self.variable_amzn = StringVar()
         self.variable_flip = StringVar()
 
-
         for word in self.product_arr:
-            if self.n == 1 :
+            if self.n == 1:
                 self.key = self.key + str(word)
                 self.n += 1
-            
+
             else:
                 self.key = self.key + '+' + str(word)
 
-
-        self.window =Toplevel(root)
+        self.window = Toplevel(root)
         self.window.title('Price Comparison Engine')
-        label_title_flip = Label(self.window, text= 'Flipkart Title:')
-        label_title_flip.grid(row=0,column=0,sticky=W)
-
+        label_title_flip = Label(self.window, text='Flipkart Title:')
+        label_title_flip.grid(row=0, column=0, sticky=W)
 
         label_flipkart = Label(self.window, text='Flipkart price (Rs):')
         label_flipkart.grid(row=1, column=0, sticky=W)
 
         entry_flipkart = Entry(self.window, textvariable=self.var_flipkart)
-        entry_flipkart.grid(row=1, column=1,sticky=W)
+        entry_flipkart.grid(row=1, column=1, sticky=W)
 
         label_title_amzn = Label(self.window, text='Amazon Title:')
         label_title_amzn.grid(row=3, column=0, sticky=W)
-
 
         label_amzn = Label(self.window, text='Amazon price (Rs):')
         label_amzn.grid(row=4, column=0, sticky=W)
 
         entry_amzn = Entry(self.window, textvariable=self.var_amzn)
-        entry_amzn.grid(row=4, column=1,sticky=W)
+        entry_amzn.grid(row=4, column=1, sticky=W)
 
         self.price_flipkart(self.key)
         self.price_amzn(self.key)
@@ -78,31 +76,32 @@ class Price_compare:
             self.variable_flip.set('Product not available')
 
         option_amzn = OptionMenu(self.window, self.variable_amzn, *self.matches_amzn)
-        option_amzn.grid(row=3,column=1,sticky=W)
+        option_amzn.grid(row=3, column=1, sticky=W)
 
         lab_amz = Label(self.window, text='Not this? Try out suggestions by clicking on the title')
-        lab_amz.grid(row=3,column=2,padx=4)
+        lab_amz.grid(row=3, column=2, padx=4)
 
         option_flip = OptionMenu(self.window, self.variable_flip, *self.matches_flip)
         option_flip.grid(row=0, column=1, sticky=W)
 
         lab_flip = Label(self.window, text='Not this? Try out suggestions by clicking on the title')
-        lab_flip.grid(row=0,column=2,padx=4)
+        lab_flip.grid(row=0, column=2, padx=4)
 
-        button_search = Button(self.window, text='Search',command=self.search,bd=4)
-        button_search.grid(row=2, column=2, sticky=E,padx=10, pady=4)
+        button_search = Button(self.window, text='Search', command=self.search, bd=4)
+        button_search.grid(row=2, column=2, sticky=E, padx=10, pady=4)
 
-        button_amzn_visit = Button(self.window, text='Visit Site', command=self.visit_amzn,bd=4)
-        button_amzn_visit.grid(row=4,column=2,sticky=W)
+        button_amzn_visit = Button(self.window, text='Visit Site', command=self.visit_amzn, bd=4)
+        button_amzn_visit.grid(row=4, column=2, sticky=W)
 
-        button_flip_visit = Button(self.window, text='Visit Site', command= self.visit_flip,bd=4)
+        button_flip_visit = Button(self.window, text='Visit Site', command=self.visit_flip, bd=4)
         button_flip_visit.grid(row=1, column=2, sticky=W)
 
+    def price_flipkart(self, key):
+        url_flip = 'https://www.flipkart.com/search?q=' + str(
+            key) + '&marketplace=FLIPKART&otracker=start&as-show=on&as=off'
 
-    def price_flipkart(self,key):
-        url_flip = 'https://www.flipkart.com/search?q=' + str(key) + '&marketplace=FLIPKART&otracker=start&as-show=on&as=off'
-
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         title_arr = []
         self.opt_title_flip = StringVar()
         source_code = requests.get(url_flip, headers=self.headers)
@@ -122,7 +121,7 @@ class Price_compare:
             for div in self.soup_flip.find_all('a', {'class': '_31qSD5'}):
                 for each in div.find_all('div', {'class': '_3wU53n'}):
                     if each.text == self.opt_title_flip.get():
-                       self.link_flip ='https://www.flipkart.com' + div.get('href')
+                        self.link_flip = 'https://www.flipkart.com' + div.get('href')
 
             product_source_code = requests.get(self.link_flip, headers=self.headers)
             product_plain_text = product_source_code.text
@@ -132,11 +131,12 @@ class Price_compare:
         except UnboundLocalError:
             pass
 
-    def price_amzn(self,key):
+    def price_amzn(self, key):
         url_amzn = 'https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + str(key)
 
         # Faking the visit from a browser
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
         # Getting titles of all products on that page
 
@@ -162,16 +162,15 @@ class Price_compare:
         for title in self.matches_amzn:
             self.looktable[title] = map[title]
         self.opt_title.set(self.matches_amzn[0])
-        self.var_amzn.set(self.looktable[self.matches_amzn[0]][0]+'.00')
+        self.var_amzn.set(self.looktable[self.matches_amzn[0]][0] + '.00')
         self.product_link = self.looktable[self.matches_amzn[0]][1]
-
 
     def search(self):
         amzn_get = self.variable_amzn.get()
         self.opt_title.set(amzn_get)
         product = self.opt_title.get()
-        price,self.product_link = self.looktable[product][0],self.looktable[product][1]
-        self.var_amzn.set(price+'.00')
+        price, self.product_link = self.looktable[product][0], self.looktable[product][1]
+        self.var_amzn.set(price + '.00')
         flip_get = self.variable_flip.get()
         self.opt_title_flip.set(flip_get)
 
@@ -194,6 +193,7 @@ class Price_compare:
 
     def visit_flip(self):
         webbrowser.open(self.link_flip)
+
 
 c = Price_compare(root)
 root.title('Price Comparison Engine')
