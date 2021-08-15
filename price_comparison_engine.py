639f6c61b3177e6d8eee4e57d8f0c3e9967af05d
Sushant <patrikarss@rknec.edu>
2021-08-16 00:51:31 +0530

    added headers for amazon

--- price_comparison_engine.py
+++ price_comparison_engine.py
@@ -4,6 +4,7 @@ import requests
 from difflib import get_close_matches
 import webbrowser
 from collections import defaultdict
+import random
 
 root = Tk()
 
@@ -135,14 +136,30 @@ class Price_compare:
 
         # Faking the visit from a browser
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+            'authority': 'www.amazon.com',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'dnt': '1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        }
 
         map = defaultdict(list)
         home = 'https://www.amazon.in'
-        source_code = requests.get(url_amzn, headers=self.headers)
+        proxies_list = ["128.199.109.241:8080", "113.53.230.195:3128", "125.141.200.53:80", "125.141.200.14:80",
+                        "128.199.200.112:138", "149.56.123.99:3128", "128.199.200.112:80", "125.141.200.39:80",
+                        "134.213.29.202:4444"]
+        proxies = {'https': random.choice(proxies_list)}
+        source_code = requests.get(url_amzn, headers=headers)
         plain_text = source_code.text
         self.opt_title = StringVar()
         self.soup = BeautifulSoup(plain_text, "html.parser")
+        # print(self.soup)
         for html in self.soup.find_all('div', {'class': 'sg-col-inner'}):
             title, link = None, None
             for heading in html.find_all('span', {'class': 'a-size-medium a-color-base a-text-normal'}):
@@ -151,6 +168,7 @@ class Price_compare:
                 price = p.text
             for l in html.find_all('a', {'class': 'a-link-normal a-text-normal'}):
                 link = home + l.get('href')
+            # print(title,link,price)
             if title and link:
                 map[title] = [price, link]
         user_input = self.var.get().title()
